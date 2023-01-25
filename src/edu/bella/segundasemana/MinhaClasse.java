3ea516db5a42cb7375eb626d8ab5167e4b4d7de9
Belisnalva Costa <bell-costa@hotmail.com.br>
2023-01-24 22:40:59 -0300

    Committing more files

--- src/edu/bella/segundasemana/MinhaClasse.java
+++ src/edu/bella/segundasemana/MinhaClasse.java
@@ -1,6 +1,16 @@
 package edu.bella.segundasemana;
+
 public class MinhaClasse {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+    public static void main(String[] args) {
+        String nome = args [0];
+        String sobrenome = args [1];
+        int idade = Integer.valueOf(args[2]);
+        double altura = Double.valueOf(args[3]);
+
+        System.out.println("Olá, me chamo " + nome + " " + sobrenome);
+        System.out.println("Tenho " + idade + " anos ");   
+        System.out.println("Minha altura é " + altura + "cm ");   
+        
     }
 }
